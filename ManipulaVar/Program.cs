dca996f098a4a45893b1b0989e0dd76a12124d89
JNeto <joaomendes15@gmail.com>
2023-03-07 19:48:50 +0000

    ManipulVar and x++ added

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ManipulaVar {
+    
+    class Program {
+        
+        static void Main(string[] args) {
+
+            try {
+                int value = int.Parse(args[0]);
+                int copy1 = value;
+                copy1++;
+                Console.WriteLine($"x++: {copy1}");
+            }
+            catch (FormatException e) {
+                Console.WriteLine("args[0] is not a valid integer number");
+            }
+
+        }
+    }
+}
