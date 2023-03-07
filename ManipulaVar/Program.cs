9f9f6bf1bc786b9cd2e0b889977215b2fccfb13a
JNeto <joaomendes15@gmail.com>
2023-03-07 19:49:34 +0000

    --x in ManipulaVar made

--- ManipulaVar/Program.cs
+++ ManipulaVar/Program.cs
@@ -9,8 +9,12 @@ namespace ManipulaVar {
             try {
                 int value = int.Parse(args[0]);
                 int copy1 = value;
+                int copy2 = value;
                 copy1++;
+                --copy2;
                 Console.WriteLine($"x++: {copy1}");
+                Console.WriteLine($"--x: {copy2}");
+                
             }
             catch (FormatException e) {
                 Console.WriteLine("args[0] is not a valid integer number");
