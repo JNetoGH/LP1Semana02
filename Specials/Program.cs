2de28d7b3d99a5d80afa0e726befd1b555f52b17
JNeto <joaomendes15@gmail.com>
2023-03-08 20:03:59 +0000

    ex 7 underflow on floating point numbers

--- Specials/Program.cs
+++ Specials/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 namespace Specials {
+    
     class Program {
 
         static void PrintCyanText(string txt) {
@@ -10,7 +11,7 @@ namespace Specials {
         }
         
         static void Main(string[] args) {
-
+            
             PrintCyanText("\nEx 6");
             Console.WriteLine($"Positive Infinity: {double.PositiveInfinity}");
             Console.WriteLine($"Negative Infinity: {double.NegativeInfinity}");
@@ -24,9 +25,15 @@ namespace Specials {
             Single decimal32Bits = Single.MaxValue * 123;
             Double decimal64Bits = (Double) Decimal.MaxValue + 2;
             Console.WriteLine($"32 bits: {decimal32Bits}");
-            Console.WriteLine($"64 bits: {decimal64Bits}");
+            Console.WriteLine($"64 bits: {decimal64Bits}\n");
             
-
+            PrintCyanText("EX 7 (Underflow Floating Point)");
+            Single singlePrecision1, singlePrecision2;
+            singlePrecision1 = singlePrecision2 = 10000000000.000003f;
+            Console.WriteLine(singlePrecision1 == singlePrecision2 + 0.00000000000001);
+            
+            
         }
+        
     }
 }
