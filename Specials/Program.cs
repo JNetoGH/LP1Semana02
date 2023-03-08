57583be0fff5f6a3bbc1bc0c17cd4d03b9bf3b24
JNeto <joaomendes15@gmail.com>
2023-03-08 16:55:09 +0000

    ex 6 done

---
+++ Specials/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Specials {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine($"{double.PositiveInfinity}");
+            Console.WriteLine($"{double.NegativeInfinity}");
+            Console.WriteLine($"{float.NaN}");
+        }
+    }
+}
