aa5e4d54818c7644ee11da4aa0190e1aa873f1c9
JNeto <joaomendes15@gmail.com>
2023-03-08 19:52:16 +0000

    decimals 32 and 64 bits added

--- Specials/Program.cs
+++ Specials/Program.cs
@@ -2,10 +2,30 @@
 
 namespace Specials {
     class Program {
+
+        static void PrintCyanText(string txt) {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(txt);
+            Console.ResetColor();
+        }
+        
         static void Main(string[] args) {
-            Console.WriteLine($"{double.PositiveInfinity}");
-            Console.WriteLine($"{double.NegativeInfinity}");
-            Console.WriteLine($"{float.NaN}");
+
+            PrintCyanText("\nEx 6");
+            Console.WriteLine($"Positive Infinity: {double.PositiveInfinity}");
+            Console.WriteLine($"Negative Infinity: {double.NegativeInfinity}");
+            Console.WriteLine($"Nan: {float.NaN}\n");
+            
+            PrintCyanText("EX 7 (Overflow Integers)");
+            Int32 signed32BitsInteger = Int32.MaxValue;
+            Console.WriteLine($"{(Int32) signed32BitsInteger + 1}\n");
+
+            PrintCyanText("EX 7 (Overflow Floating Point)");
+            Single decimal32Bits = Single.MaxValue * 123;
+            Double decimal64Bits = (Double) Decimal.MaxValue + 2;
+            Console.WriteLine($"32 bits: {decimal32Bits}");
+            Console.WriteLine($"64 bits: {decimal64Bits}");
+
         }
     }
 }
