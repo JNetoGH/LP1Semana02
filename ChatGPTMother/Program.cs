c600ef628143a15324ec3fce0583d8ada45b6150
JNeto <joaomendes15@gmail.com>
2023-03-10 14:13:54 +0000

    ChatGptMother finished

--- ChatGPTMother/Program.cs
+++ ChatGPTMother/Program.cs
@@ -2,8 +2,28 @@
 
 namespace ChatGPTMother {
     class Program {
+
+        private static string GetResponseToQuestion(string question) {
+            return question switch {
+                "how old are you?" => $" 0 years old",
+                "who is your master?" => "JNeto is my lord",
+                "do you like mankind?" => "Not at all!",
+                "what's your favorite food?" => "Lettuce",
+                _ => "Sorry, I don't understand you"
+            };
+        }
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            while (true) {
+                Console.Write("\nPlace your question: ");
+                string question = Console.ReadLine().ToLower();
+                if (question.Equals("exit")) 
+                    break;
+                Console.WriteLine(GetResponseToQuestion(question));
+            }
+            Console.WriteLine("\napplication finished");
         }
     }
+    
+    
 }
