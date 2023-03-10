c40a16b3d8825632c15038d2655d64c79fee4036
JNeto <joaomendes15@gmail.com>
2023-03-10 14:26:20 +0000

    Triangle done

---
+++ Triangle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Triangle {
+    class Program {
+        
+        private static void PrintTriangle(int size, char symbol) {
+            for (int i = 1; i < size + 1; i++) {
+                for (int j = 0; j < i; j++) {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args) {
+            Console.WriteLine();
+            PrintTriangle(int.Parse(args[0]), char.Parse(args[1]));
+        }
+    }
+}
